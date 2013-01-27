36efea348c13386ec24d03ceddccba1af455ec76
Alex Home <alex@blackbirdinteractive.com>
2013-01-26 20:40:15 -0800

    updated controls for heart rate / rotation

--- game/Assets/Scripts/InputControl.cs
+++ game/Assets/Scripts/InputControl.cs
@@ -5,13 +5,10 @@ public class InputControl : MonoBehaviour {
 	
 	protected Animator animator;	
 	public float DirectionDampTime = .25f;
-	public bool ApplyGravity = true;
-	public int HeartRate = 80;
-	public int ForwardSpeed = 50;
-	public int LeftSideSpeed = 0;
-	public int RightSideSpeed = 0;
+	public float HeartRate = 0.01f;
+	public float TurnSpeed;
+	private float mRotation = 0;
 	public int HeartRateChangeRate = 0;
-	[SerializeField] private int mDistanceTravelled = 0;
 	
 	// Use this for initialization
 	void Start() 
@@ -27,10 +24,6 @@ public class InputControl : MonoBehaviour {
 	{
 		if (animator)
 		{	
-			float v = 1.0f;
-			float h = 0.0f;
-			
-			animator.SetFloat("Speed", h*h+v*v);
 			
 			AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);			
 			
@@ -48,25 +41,23 @@ public class InputControl : MonoBehaviour {
 						
 			if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
 			{
-				animator.SetFloat("Direction", -1, DirectionDampTime, Time.deltaTime);	
-				LeftSideSpeed = LeftSideSpeed + 2;
-				RightSideSpeed = 0;
+				mRotation = -1;
 			}
 			else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
 			{
-				animator.SetFloat("Direction", 1, DirectionDampTime, Time.deltaTime);
-				RightSideSpeed = RightSideSpeed + 2;
-				LeftSideSpeed = 0;
+				mRotation = 1;
 			}
 			else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
 			{				
-				LeftSideSpeed = 0;
-				RightSideSpeed = 0;
-				animator.SetFloat("Direction", 0, 0, Time.deltaTime);				
+				mRotation = 0;
 			}
+			Debug.Log(mRotation);
 			
-			mDistanceTravelled = Mathf.FloorToInt(transform.position.z);
-			rigidbody.MovePosition(transform.position + (transform.forward * ForwardSpeed + transform.right * RightSideSpeed + (-transform.right) * LeftSideSpeed) * Time.deltaTime);
+			animator.SetFloat("Speed", HeartRate);
+			animator.SetFloat("Direction", mRotation, DirectionDampTime, Time.deltaTime);	
+			rigidbody.rotation.Set(transform.rotation.x, transform.rotation.y + mRotation * TurnSpeed, transform.rotation.z, transform.rotation.w);
+			Debug.Log(rigidbody.rotation);
+			rigidbody.MovePosition(transform.position + transform.forward * HeartRate * Time.deltaTime);
 		}   		  
 	}
 }
