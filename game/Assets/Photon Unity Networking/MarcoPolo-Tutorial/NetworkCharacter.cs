e8a5c5a6c7a7afa9c505f6bc6544047721d96b4f
Devin Kavanagh <devin.kav@gmail.com>
2013-01-27 13:32:25 -0800

    network updates

--- game/Assets/Photon Unity Networking/MarcoPolo-Tutorial/NetworkCharacter.cs
+++ game/Assets/Photon Unity Networking/MarcoPolo-Tutorial/NetworkCharacter.cs
@@ -7,13 +7,14 @@ public class NetworkCharacter : Photon.MonoBehaviour
     private Vector3 correctPlayerPos = Vector3.zero; // We lerp towards this
     private Quaternion correctPlayerRot = Quaternion.identity; // We lerp towards this
 	public Camera cam = null;
-	
 	private bool mIsReady = false;
 	private static bool mGameOn = false;
 	public static Dictionary<int, NetworkCharacter> Characters = null;
+	
 		
     public void Awake()
     {
+		
 	//ThirdPersonController myC = GetComponent<ThirdPersonController>();
        // myC.isControllable = photonView.isMine;
 		if (Characters == null)
@@ -71,6 +72,8 @@ public class NetworkCharacter : Photon.MonoBehaviour
         if (stream.isWriting)
         {
             // We own this player: send the others our data
+			//stream.SendNext(photonView.viewID.ID);
+			stream.SendNext(mIsReady);
             stream.SendNext(transform.position);
             stream.SendNext(transform.rotation);
 			stream.SendNext(rigidbody.velocity);
@@ -80,10 +83,25 @@ public class NetworkCharacter : Photon.MonoBehaviour
         else
         {
             // Network player, receive data
+			//int id = (int)stream.ReceiveNext();
+			mIsReady = (bool)stream.ReceiveNext();
             this.correctPlayerPos = (Vector3)stream.ReceiveNext();
             this.correctPlayerRot = (Quaternion)stream.ReceiveNext();
 			rigidbody.velocity = (Vector3)stream.ReceiveNext();
-			
+			/*
+			if (Characters != null)
+			{
+				if (Characters.ContainsKey(id))
+				{
+					Characters[id].mIsReady = true;
+				}
+				else
+				{
+					Characters.Add(id);
+					Characters[id].mIsReady = true;
+				}
+					
+			}*/
 			//GetComponent<InputControl>().HeartRate = (float)stream.ReceiveNext();
         }
 		
@@ -99,6 +117,7 @@ public class NetworkCharacter : Photon.MonoBehaviour
 					readyCount++;
 				}
 			}
+			Debug.Log(readyCount);
 			if (readyCount == playerCount)
 			{
 				mGameOn = true;
