536b106b60a517c5dff318c0b2155538aa73c8f3
Rocky He <rocky.jj.he@hotmail.com>
2013-01-26 20:11:58 -0800

    Updated InputControl for new control behaviour

--- game/Assets/Scripts/InputControl.cs
+++ game/Assets/Scripts/InputControl.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class InputControl : MonoBehaviour {
 	
-	protected Animator animator;
+	protected Animator animator;	
+	public float DirectionDampTime = .25f;
 	public bool ApplyGravity = true;
-	public float ForwardSpeed = 1.0f;
-	public float LeftSideSpeed = 0.0f;
-	public float RightSideSpeed = 0.0f;
-	[SerializeField] private float DistanceTravelled = 0.0f;
+	public int HeartRate = 80;
+	public int ForwardSpeed = 50;
+	public int LeftSideSpeed = 0;
+	public int RightSideSpeed = 0;
+	public int HeartRateChangeRate = 0;
+	[SerializeField] private int mDistanceTravelled = 0;
 	
 	// Use this for initialization
 	void Start() 
@@ -27,6 +30,8 @@ public class InputControl : MonoBehaviour {
 			float v = 1.0f;
 			float h = 0.0f;
 			
+			animator.SetFloat("Speed", h*h+v*v);
+			
 			AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);			
 			
 			if (stateInfo.IsName("Base Layer.Run"))
@@ -34,75 +39,33 @@ public class InputControl : MonoBehaviour {
 				if (Input.GetKey(KeyCode.Space))
 				{
 					animator.SetBool("Jump", true);
-					if ((ForwardSpeed - 1) > 0)
-					{
-						ForwardSpeed = ForwardSpeed * 0.5f;
-					}
 				}
-				else if (Input.GetKey(KeyCode.LeftShift))
-				{
-					animator.SetBool("Dive", true);
-					if ((ForwardSpeed - 1) > 0)
-					{
-						ForwardSpeed = ForwardSpeed * 0.5f;
-					}
-				}		
 			}
 			else
 			{
 				animator.SetBool("Jump", false);
-				animator.SetBool("Dive", false);
-            }		
-			
-			if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-			{
-				v = v * 1.1f;
-				animator.SetFloat("Speed", h*h+v*v);
-				
-				ForwardSpeed = ForwardSpeed + 7.0f;
-			}		
-			/*
-			else if (Input.GetKey(KeyCode.S))
-			{
-				v = v * 1.1f;			
-				animator.SetFloat("Speed", h*h+v*v);
-				
-				if( ForwardSpeed - 5 > 0)
-				{
-					ForwardSpeed = ForwardSpeed - 5;
-				}
 			}
-			*/
 						
 			if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
 			{
-				animator.SetFloat("Direction", 0, 0, Time.deltaTime);	
-				LeftSideSpeed = LeftSideSpeed + 2.0f;
-				RightSideSpeed = 0.0f;
+				animator.SetFloat("Direction", -1, DirectionDampTime, Time.deltaTime);	
+				LeftSideSpeed = LeftSideSpeed + 2;
+				RightSideSpeed = 0;
 			}
 			else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
 			{
-				animator.SetFloat("Direction", 0, 0, Time.deltaTime);
-				RightSideSpeed = RightSideSpeed + 2.0f;
-				LeftSideSpeed = 0.0f;
+				animator.SetFloat("Direction", 1, DirectionDampTime, Time.deltaTime);
+				RightSideSpeed = RightSideSpeed + 2;
+				LeftSideSpeed = 0;
 			}
 			else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
 			{				
-				LeftSideSpeed = 0.0f;
-				RightSideSpeed = 0.0f;
+				LeftSideSpeed = 0;
+				RightSideSpeed = 0;
+				animator.SetFloat("Direction", 0, 0, Time.deltaTime);				
 			}
 			
-			if (ForwardSpeed > 0 )
-			{
-				ForwardSpeed = ForwardSpeed*0.95f;
-				
-				if (ForwardSpeed < 1)
-				{
-					ForwardSpeed = 0;
-				}				
-			}
-			transform.position += transform.forward * ForwardSpeed * Time.deltaTime;
-			DistanceTravelled = transform.position.z;
+			mDistanceTravelled = Mathf.FloorToInt(transform.position.z);
 			rigidbody.MovePosition(transform.position + (transform.forward * ForwardSpeed + transform.right * RightSideSpeed + (-transform.right) * LeftSideSpeed) * Time.deltaTime);
 		}   		  
 	}
